69b198a00316da155e8a96593f4b18a6d730f572
xuexcy <xuechengyunxue@gmail.com>
2024-07-04 22:32:24 +0800

    add 6.3

---
+++ xcy-solutions/6. 复合类型/6.3/main1.rs
@@ -0,0 +1,17 @@
+
+// 修复代码中的错误，不要新增代码行!
+// fn main() {
+//     let arr = [1, 2, 3];
+//     let s1: [i32] = arr[0..2];
+//
+//     let s2: str = "hello, world" as str;
+// }
+
+
+// 修复代码中的错误，不要新增代码行!
+fn main() {
+    let arr = [1, 2, 3];
+    let s1: &[i32] = &arr[0..2];
+
+    let s2: &str = "hello, world" as &str;
+}
---
+++ xcy-solutions/6. 复合类型/6.3/main5.rs
@@ -0,0 +1,18 @@
+
+// fn main() {
+//     let s = "你好，世界";
+//     // 修改以下代码行，让代码工作起来
+//     let slice = &s[0..2];
+//
+//     assert!(slice == "你");
+// }
+
+
+fn main() {
+    let s = "你好，世界";
+    // 修改以下代码行，让代码工作起来
+    // utf8
+    let slice = &s[0..3];
+
+    assert!(slice == "你");
+}
