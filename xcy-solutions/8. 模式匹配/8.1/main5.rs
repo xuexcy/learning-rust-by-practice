d44e939fd20527aef51279a533045e7406edb561
xuexcy <xuechengyunxue@gmail.com>
2024-07-05 22:19:26 +0800

    add 8.1

---
+++ xcy-solutions/8. 模式匹配/8.1/main5.rs
@@ -0,0 +1,37 @@
+
+// enum MyEnum {
+//     Foo,
+//     Bar
+// }
+//
+// fn main() {
+//     let mut count = 0;
+//
+//     let v = vec![MyEnum::Foo,MyEnum::Bar,MyEnum::Foo];
+//     for e in v {
+//         if e == MyEnum::Foo { // 修复错误，只能修改本行代码
+//             count += 1;
+//         }
+//     }
+//
+//     assert_eq!(count, 2);
+// }
+
+
+enum MyEnum {
+    Foo,
+    Bar
+}
+
+fn main() {
+    let mut count = 0;
+
+    let v = vec![MyEnum::Foo,MyEnum::Bar,MyEnum::Foo];
+    for e in v {
+        if matches!(e, MyEnum::Foo) { // 修复错误，只能修改本行代码
+            count += 1;
+        }
+    }
+
+    assert_eq!(count, 2);
+}
---
+++ xcy-solutions/8. 模式匹配/8.1/main8.rs
@@ -0,0 +1,37 @@
+
+// enum Foo {
+//     Bar,
+//     Baz,
+//     Qux(u32)
+// }
+//
+// fn main() {
+//     let a = Foo::Qux(10);
+//
+//     // 移除以下代码，使用 `match` 代替
+//     if let Foo::Bar = a {
+//         println!("match foo::bar")
+//     } else if let Foo::Baz = a {
+//         println!("match foo::baz")
+//     } else {
+//         println!("match others")
+//     }
+// }
+
+
+enum Foo {
+    Bar,
+    Baz,
+    Qux(u32)
+}
+
+fn main() {
+    let a = Foo::Qux(10);
+
+    // 移除以下代码，使用 `match` 代替
+    match a {
+        Foo::Bar => println!("match foo::bar"),
+        Foo::Baz => println!("match foo::baz"),
+        _ => println!("match others"),
+    }
+}
