a758c75ec6db2767c7ca5e62818437aeb2acf254
xuexcy <xuechengyunxue@gmail.com>
2024-07-05 17:21:55 +0800

    add 6.2

---
+++ xcy-solutions/6. 复合类型/6.2/main3.rs
@@ -0,0 +1,17 @@
+
+// fn main() {
+//     // 填空
+//     let list: [i32; 100] = __ ;
+//
+//     assert!(list[0] == 1);
+//     assert!(list.len() == 100);
+// }
+
+
+fn main() {
+    // 填空
+    let list: [i32; 100] = [1; 100] ;
+
+    assert!(list[0] == 1);
+    assert!(list.len() == 100);
+}
---
+++ xcy-solutions/6. 复合类型/6.2/main5.rs
@@ -0,0 +1,17 @@
+
+// fn main() {
+//     let arr = ['a', 'b', 'c'];
+//
+//     let ele = arr[1]; // 只修改此行来让代码工作
+//
+//     assert!(ele == 'a');
+// }
+
+
+fn main() {
+    let arr = ['a', 'b', 'c'];
+
+    let ele = arr[0]; // 只修改此行来让代码工作
+
+    assert!(ele == 'a');
+}
