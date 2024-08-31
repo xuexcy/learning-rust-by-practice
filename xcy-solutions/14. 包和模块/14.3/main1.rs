ac2b7036a2647d774aaf1a7f06d519f21ccdb6e1
xuexcy <xuechengyunxue@gmail.com>
2024-08-31 15:54:59 +0800

    add 14.3

---
+++ xcy-solutions/14. 包和模块/14.3/main1.rs
@@ -0,0 +1,9 @@
+// use std::fmt::Result;
+// use std::io::Result;
+//
+// fn main() {}
+
+use std::fmt::Result as FmtResult;
+use std::io::Result as IoResult;
+
+fn main() {}
---
+++ xcy-solutions/14. 包和模块/14.3/main2.rs
@@ -0,0 +1,24 @@
+//
+// // 使用两种方式填空
+// // 不要添加新的代码行
+// use std::collections::__;
+//
+// fn main() {
+//     let _c1:HashMap<&str, i32> = HashMap::new();
+//     let mut c2 = BTreeMap::new();
+//     c2.insert(1, "a");
+//     let _c3: HashSet<i32> = HashSet::new();
+// }
+
+
+// 使用两种方式填空
+// 不要添加新的代码行
+//use std::collections::{BTreeMap, HashMap, HashSet};
+use std::collections::*;
+
+fn main() {
+    let _c1:HashMap<&str, i32> = HashMap::new();
+    let mut c2 = BTreeMap::new();
+    c2.insert(1, "a");
+    let _c3: HashSet<i32> = HashSet::new();
+}
